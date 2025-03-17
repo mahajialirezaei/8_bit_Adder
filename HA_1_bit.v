80bce2b5ba69ec09d49b677d9619cab610115954
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-03-17 03:57:15 +0330

    Ha edited rm semi callen

--- HA_1_bit.v
+++ HA_1_bit.v
@@ -2,10 +2,10 @@ module HA_1_bit(
     input a,
     input b,
     output sum_ha,
-    output co_ha;
+    output co_ha
 );
 
 assign sum_ha = a ^ b;
-assign co_ha = a & b;
+assign co_ha = a && b;
 
 endmodule
